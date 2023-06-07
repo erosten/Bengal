93334b5a77b08ee63abe30877e1f6e4a640662c3
Erik <erikrrosten@gmail.com>
2023-06-07 04:45:00 -0700

    uncomment mate in 3s

--- pytest_tests/test_search.py
+++ pytest_tests/test_search.py
@@ -3,19 +3,20 @@ from tqdm import tqdm
 from .. import Board, Move
 from .. import Searcher as PVSearcher
 
-# def test_mate_in_threes(m8in3_fens):
-#     mates = 0
-#     for fen in tqdm(m8in3_fens):
-#         board = Board(fen)
-#         s = PVSearcher()
-#         score, moves = s.find_move(board, depth=5)
-#         for m in moves:
-#             board.push(Move.from_uci(m))
 
-#         if board.is_checkmate():
-#             mates += 1
+def test_mate_in_threes(m8in3_fens):
+    mates = 0
+    for fen in tqdm(m8in3_fens):
+        board = Board(fen)
+        s = PVSearcher()
+        score, moves = s.find_move(board, depth=5)
+        for m in moves:
+            board.push(Move.from_uci(m))
 
-#     assert len(m8in3_fens) - m8in3_fens < 40
+        if board.is_checkmate():
+            mates += 1
+
+    assert len(m8in3_fens) - m8in3_fens < 40
 
 
 def test_mate_in_twos(m8in2_fens):
